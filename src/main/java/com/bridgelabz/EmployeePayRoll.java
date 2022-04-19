34ebec4565bcd2e0e831e48e843b2bebaf124b24
praju <sawantprajakta84@gmail.com>
2022-04-19 12:27:28 +0530

    Wel come to Employee pay roll

---
+++ src/main/java/com/bridgelabz/EmployeePayRoll.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+
+public class EmployeePayRoll {
+    public static void main(String[] args) {
+        System.out.println("Wel come to Employee pay roll");
+    }
+}
